a31c384cc884a4148d9ad66522a75bd01d2fdcf5
gustavobat989 <141661080+gustavobat989@users.noreply.github.com>
2024-11-19 08:28:43 -0300

    Add files via upload

---
+++ peguntas.js/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Sociedade',
+    'Qual a pessoa mais rica de Juranda?',
+    'A pessoa mais rica de Juranda e o Fernando Moreira.'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual o maior artilheiro do society?',
+    'O maior aritlheiro do society e o Gustavo Batista de Almeida Valentim'
+)
+
+criaCartao(
+    'Qual o maior pescador da região de Goioere?',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
